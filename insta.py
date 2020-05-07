c4ebeb3fd9da989974acee74754fb59685945b3f
EraldoJuunior <eraldojuniorbmxx1@hotmail.com>
2020-05-07 03:37:57 -0400

    subindo

--- insta.py
+++ insta.py
@@ -4,7 +4,7 @@ browser = webdriver.Chrome()
 browser.get("https://www.instagram.com/?hl=pt-br")
 elem = browser.find_element_by_name("username")
 
-elem.send_keys("llh")
+elem.send_keys("ll")
 
 elem.submit()
 
