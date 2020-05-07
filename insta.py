44167acde78a3ef835de6a59c375f9a7f424877a
EraldoJuunior <eraldojuniorbmxx1@hotmail.com>
2020-05-07 00:41:05 -0400

    subindo

--- insta.py
+++ insta.py
@@ -1,4 +1,13 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
 browser = webdriver.Chrome()
-browser.get('http://www.ubuntu.com/')
+browser.get("http://www.python.org")
+elem = browser.find_element_by_id("id-search-field")
+elem.send_keys('1')
+
+
+elem.send_keys(Keys.ENTER)
+elem.submit()
+
+browser.close()
+
